e66c890993ff911b067735e208228fc6bd665cd5
iabuitham <issaabuithamfilm@gmail.com>
2026-04-22 16:13:59 +0300

    Edited backend logic

--- submit.php
+++ submit.php
@@ -1,16 +1,27 @@
 <?php
 include 'db.php';
 
-// Basic validation
 $name   = trim($_POST['name']);
 $phone  = trim($_POST['phone']);
 $guests = intval($_POST['guests']);
 
-if ($name === "" || $phone === "" || $guests <= 0) {
+// cleaning
+$phone = preg_replace('/\s+/', '', $phone); // remove spaces
+
+// ensure it starts with 0
+if (substr($phone, 0, 1) === "0") {
+    $phone = substr($phone, 1);
+}
+
+// add Jordan country code
+$phone = "+00962" . $phone;
+
+// validation
+if ($name === "" || $phone === "+00962" || $guests <= 0) {
     die("Invalid input");
 }
 
-// Insert
+// insert
 $sql = "INSERT INTO reservations (name, phone, guests) VALUES (?, ?, ?)";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("ssi", $name, $phone, $guests);
